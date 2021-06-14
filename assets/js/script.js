a826303ae929f61f006c22ed72d0757d6daae5bc
PatriciaLocatelli <patriciawalau@hotmail.com>
2021-06-14 00:30:35 -0300

    Falta terminar os bullets carrossel e resposivo

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#carrossel-depoimentos").owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true,
+    autoPlayTimeout: 5000,
+    navigation: true
+  });
+});
