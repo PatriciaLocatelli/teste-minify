b258fb4b78db8b9d4a79a0aab4ade490493191bf
PatriciaLocatelli <patriciawalau@hotmail.com>
2021-06-14 09:08:25 -0300

    AJustado o Carrossel, terminado o responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,30 @@ $(document).ready(function () {
     loop: true,
     autoplay: true,
     autoPlayTimeout: 5000,
-    navigation: true
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
   });
 });
+
+function myFunction() {
+  var x = document.getElementById("menu-mobile");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
